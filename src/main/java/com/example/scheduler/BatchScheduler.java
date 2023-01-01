181cb5a9e1cc17c713386dfde7f2ee33dbf77ed2
dlalscjf94 <ssw01092@naver.com>
2023-01-02 02:11:42 +0900

    feat : mysql local 설정 등록 및 test 용 BatchScheduler 클래스 생성

---
+++ src/main/java/com/example/scheduler/BatchScheduler.java
@@ -0,0 +1,21 @@
+package com.example.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+
+@Component
+public class BatchScheduler {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    // 10초마다 실행
+    @Scheduled(cron = "0/10 * * * * *", zone = "Asia/Seoul")
+    public void testSchedule() {
+        logger.info("this is test schedule {}", LocalDateTime.now());
+    }
+}
